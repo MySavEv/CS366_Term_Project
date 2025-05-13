c9d22b31c10b1d6a9c8f57cbeae674cc707c3c34
MySavEv <save.oomyim568@gmail.com>
2025-05-13 18:49:09 +0700

    Add KafkaListener RiderEvent

--- Notification-service/src/main/java/com/cs366/notification/config/KafkaConsumerConfig.java
+++ Notification-service/src/main/java/com/cs366/notification/config/KafkaConsumerConfig.java
@@ -15,6 +15,9 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
+import com.cs366.notification.event.RiderAssignedEvent;
+import com.cs366.notification.event.RiderDeliveredFoodEvent;
+import com.cs366.notification.event.RiderPickedUpFoodEvent;
 import com.cs366.notification.event.UserEventPayload;
 
 
@@ -22,30 +25,66 @@ import com.cs366.notification.event.UserEventPayload;
 @EnableKafka
 public class KafkaConsumerConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    private final Map<String,Object> props = new HashMap<>();
 
-    @Bean
-    public ConsumerFactory<String, UserEventPayload> userFactory() {
-        Map<String, Object> props = new HashMap<>();
+    KafkaConsumerConfig(){
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "noti-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.cs366.restaurant.event"); // หรือระบุเฉพาะ package ที่ใช้งาน
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.cs366.notification.event"); // หรือระบุเฉพาะ package ที่ใช้งาน
+    }
 
-        return new DefaultKafkaConsumerFactory<>(
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, UserEventPayload> userKafkaListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, UserEventPayload> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(
             props,
             new StringDeserializer(),
             new JsonDeserializer<>(UserEventPayload.class)
-        );
+        ));
+        return factory;
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, UserEventPayload> orderDetailKafkaListenerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, UserEventPayload> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, RiderAssignedEvent> riderAssignedKafkaListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, RiderAssignedEvent> factory =
             new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(userFactory());
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(
+            props,
+            new StringDeserializer(),
+            new JsonDeserializer<>(RiderAssignedEvent.class)
+        ));
         return factory;
     }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, RiderDeliveredFoodEvent> riderDeliveredKafkaListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, RiderDeliveredFoodEvent> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(
+            props,
+            new StringDeserializer(),
+            new JsonDeserializer<>(RiderDeliveredFoodEvent.class)
+        ));
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, RiderPickedUpFoodEvent> riderPickedKafkaListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, RiderPickedUpFoodEvent> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(
+            props,
+            new StringDeserializer(),
+            new JsonDeserializer<>(RiderPickedUpFoodEvent.class)
+        ));
+        return factory;
+    }
+
+
 }
--- Notification-service/src/main/java/com/cs366/notification/kafka/KafkaConsumer.java
+++ Notification-service/src/main/java/com/cs366/notification/kafka/KafkaConsumer.java
@@ -3,19 +3,42 @@ package com.cs366.notification.kafka;
 
 import org.springframework.kafka.annotation.KafkaListener;
 
+import com.cs366.notification.event.RiderAssignedEvent;
+import com.cs366.notification.event.RiderDeliveredFoodEvent;
+import com.cs366.notification.event.RiderPickedUpFoodEvent;
 import com.cs366.notification.event.UserEventPayload;
 import com.cs366.notification.service.NotiService;
 
 public class KafkaConsumer {
-    @KafkaListener(topics = "user.registered")
+    @KafkaListener(topics = "user.registered", containerFactory = "userKafkaListenerFactory")
     public void userRegister(UserEventPayload payload) {
         // Print statement
         NotiService.sendNoti(payload.getFullName() + "Registered");
     }
 
-    @KafkaListener(topics = "user.registered")
+    @KafkaListener(topics = "user.registered", containerFactory = "userKafkaListenerFactory")
     public void userLogin(UserEventPayload payload) {
         // Print statement
         NotiService.sendNoti(payload.getFullName() + "Login!");
     }
+
+    @KafkaListener(topics = "rider-assigned", containerFactory = "riderAssignedKafkaListenerFactory")
+    public void riderAssigned(RiderAssignedEvent payload) {
+        // Print statement
+        NotiService.sendNoti(payload.getRiderId() + "Login!");
+    }
+
+    @KafkaListener(topics = "rider-delivered", containerFactory = "riderDeliveredKafkaListenerFactory")
+    public void riderDelivered(RiderDeliveredFoodEvent payload) {
+        // Print statement
+        NotiService.sendNoti(payload.getRiderId() + "Login!");
+    }
+
+    @KafkaListener(topics = "rider-picked-up", containerFactory = "riderPickedKafkaListenerFactory")
+    public void riderPicked(RiderPickedUpFoodEvent payload) {
+        // Print statement
+        NotiService.sendNoti(payload.getRiderId() + "Login!");
+    }
+
+
 }
