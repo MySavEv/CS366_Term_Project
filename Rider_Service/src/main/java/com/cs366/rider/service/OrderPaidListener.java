d5d8cd47550ae6f32735b7d7fabc8453b29579da
MySavEv <save.oomyim568@gmail.com>
2025-05-13 23:14:17 +0700

    rix bug

--- Rider_Service/src/main/java/com/cs366/rider/service/OrderPaidListener.java
+++ Rider_Service/src/main/java/com/cs366/rider/service/OrderPaidListener.java
@@ -1,23 +1,22 @@
 package com.cs366.rider.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class OrderPaidListener {
+    @Autowired
+    private RiderService riderService;
 
-    @KafkaListener(topics = "order-paid", groupId = "rider-group", containerFactory = "kafkaListenerContainerFactory")
+    
+
+    @KafkaListener(topics = "orderpaid", groupId = "rider-group", containerFactory = "kafkaListenerContainerFactory")
     public void handleOrderPaid(String message) {
         System.out.println("📦 Received OrderPaid Event: Order ID " + message);
 
         // Mock หา Rider
-        String rider = findAvailableRider();
-        System.out.println("🛵 Assigned Rider: " + rider + " for Order " + message);
-    }
-
-    private String findAvailableRider() {
-        // Mock logic หา driver
-        return "Rider#" + (int)(Math.random() * 1000);
+        riderService.assignRider(message);
     }
 }
