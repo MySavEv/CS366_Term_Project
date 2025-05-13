56b7d7d7d5246c4985f0d50a638b8e7d80778f19
MySavEv <save.oomyim568@gmail.com>
2025-05-13 18:53:24 +0700

    fix hardcode bootstrapServer

--- Rider_Service/src/main/java/com/cs366/rider/config/KafkaProducerConfig.java
+++ Rider_Service/src/main/java/com/cs366/rider/config/KafkaProducerConfig.java
@@ -16,11 +16,14 @@ import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
 
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
